82fc6f7a00258122037c9d9c7e3f1aaaf3b2abc2
Ledda Bryan <louchebem06@gmail.com>
2022-03-23 18:37:03 +0100

    Generate automatic table of db docker-compose

---
+++ init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Testos
+(
+    id numeric,
+    name character varying[],
+    ok boolean,
+    PRIMARY KEY (id)
+);
+CREATE TABLE Testos4242
+(
+    id numeric,
+    name character varying[],
+    ok boolean,
+    PRIMARY KEY (id)
+);
