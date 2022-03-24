665a09f79f7ac06c1c834ea83f2611e9e6394c67
Bryan Ledda <bledda@c1r3p2.42nice.fr>
2022-03-24 17:52:36 +0100

    up

--- init.sql
+++ init.sql
@@ -1,11 +1,4 @@
-CREATE TABLE Testos
-(
-    id numeric,
-    name character varying[],
-    ok boolean,
-    PRIMARY KEY (id)
-);
-CREATE TABLE Testos4242
+CREATE TABLE public.Testos
 (
     id numeric,
     name character varying[],
