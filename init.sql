88ce271f23810e771d219cac1031642065bb4d73
Ledda Bryan <louchebem06@gmail.com>
2022-03-24 22:30:23 +0100

    Update: add base table

--- init.sql
+++ init.sql
@@ -1,7 +1,42 @@
 CREATE TABLE public."Testos"
 (
-    id numeric,
-    name character varying[],
-    ok boolean,
+    id 		numeric,
+    name	character varying[],
+    ok 		boolean,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE public."Users"
+(
+	id 			numeric,
+	fullname 	character varying[],
+	pseudo 		character varying[],
+	twoauth		boolean,
+	img			character varying[]
+);
+
+CREATE TABLE public."Matchs"
+(
+	id 		numeric,
+	player1 numeric,
+	player2 numeric,
+	victory numeric,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE public."Channels"
+(
+	id 			numeric,
+	users 		numeric[],
+	name 		character varying[],
+	password 	character varying[],
+	grade 		character varying[],
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE public."Messages"
+(
+	id_user 	numeric,
+	id_channel 	numeric,
+	msg 		character varying[]
 );
