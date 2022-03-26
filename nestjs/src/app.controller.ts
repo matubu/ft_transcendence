8928827c8e824875570912338140385e1bd41a1b
Bryan Ledda <bledda@c1r3p2.42nice.fr>
2022-03-26 16:50:56 +0100

    update

--- nestjs/src/app.controller.ts
+++ nestjs/src/app.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Get, Req, Response } from '@nestjs/common';
 import { AppService } from './app.service';
-import { FastifyRequest } from 'fastify';
+// import { FastifyRequest } from 'fastify';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(@Req() req: FastifyRequest): string {
-	console.log(req.unsignCookie(req.cookies.user));
+  getHello(/*@Req() req: FastifyRequest*/): string {
+	// console.log(req.unsignCookie(req.cookies.user));
     return this.appService.getHello();
   }
 }
--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import fastifyCookie from 'fastify-cookie';
 
-const { NESTJS_PORT } = process.env
+const { NESTJS_PORT, SECRET_COOKIES } = process.env
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestFastifyApplication>(
@@ -16,8 +16,9 @@ async function bootstrap() {
 		origin: "http://localhost",
 		credentials: true,
 	});
+	const secret = SECRET_COOKIES;
 	app.register(fastifyCookie, {
-		secret: '12345'
+		secret: `${secret}`
 	});
 	await app.listen(port, () => console.log(`Server started on http://localhost:${port}`));
   }
