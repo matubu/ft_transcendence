3804b2ebf98359e9406ece6442ad962d56b644ed
Bryan Ledda <bledda@c1r3p2.42nice.fr>
2022-03-27 12:53:01 +0200

    FIX: scripte create user in school

--- createUser.sh
+++ createUser.sh
@@ -1,37 +1,64 @@
 #!/bin/bash
-MAX=5
+MAX=10
 
 echo -en "Check jq "
 if [ $(brew list | grep jq) != "jq" ]
 then
-	brew install jq &> /dev/null
-	echo "[installed]"
+	echo "Install jq please"
+	exit
 else
 	echo "[ok]"
 fi
 
 echo "STEP 1/3"
-for (( c=1; c<=$MAX; c++ ))
-do
-	echo -en "\t Generate FakeUser : $c\\$MAX\r"
-	FAKENAME=$(curl --silent https://api.namefake.com/)
-	NAME+=($(printf "%s" $FAKENAME | jq .name))
-	NICKNAME+=($(printf "%s" $FAKENAME | jq .username))
-done
+if [ ! -f ./fakeuser.json ]
+then
+	echo "[" > fakeuser.json
+	for (( c=1; c<=$MAX; c++ ))
+	do
+		if (( c!=1 ))
+		then
+			echo "," >> fakeuser.json
+		fi
+		echo -en "\t Generate FakeUser : $c\\$MAX\r"
+		FAKENAME=$(curl --silent https://api.namefake.com/ --insecure)
+		echo $FAKENAME >> fakeuser.json
+		NAME+=($(printf "%s" $FAKENAME | jq .name))
+		NICKNAME+=($(printf "%s" $FAKENAME | jq .username))
+	done
+	echo "]" >> fakeuser.json
+else
+	echo "[ok]"
+fi
 
 echo -e "\nSTEP 2/3"
-for (( c=1; c<=$MAX; c++ ))
-do
-	echo -en "\t Generate Image : $c\\$MAX\r"
-	REQ=$(curl --silent https://aws.random.cat/meow)
-	PICTURE+=($(printf "$s\n" $REQ | jq .file))
-done
+if [ ! -f ./image.json ]
+then
+	echo "[" > image.json
+	for (( c=1; c<=$MAX; c++ ))
+	do
+		if (( c!=1 ))
+		then
+			echo "," >> image.json
+		fi
+		echo -en "\t Generate Image : $c\\$MAX\r"
+		REQ=$(curl --silent https://aws.random.cat/meow)
+		echo $REQ >> image.json
+		PICTURE+=($(echo $REQ | jq .file))
+	done
+	echo "]" >> image.json
+else
+	echo "[ok]"
+fi
 
 echo -e "\nSTEP 3/3"
 for (( c=1; c<=$MAX; c++ ))
 do
 	echo -en "\t Upload in DataBase : $c\\$MAX\r"
-	TMP={"\"id\"":"$(( $RANDOM + 10000 ))","\"fullname\"":"${NAME[$c - 1]}","\"nickname\"":"${NICKNAME[$c - 1]}","\"twoauth\"":false,"\"img\"":"${PICTURE[$c - 1]}","\"elo\"":$RANDOM}
+	NAME=$(cat ./fakeuser.json | jq .[$c-1].name)
+	NICKNAME=$(cat ./fakeuser.json | jq .[$c-1].username)
+	PICTURE=$(cat image.json | jq ".[$c-1].file")
+	TMP={"\"id\"":"$(( $RANDOM + 10000 ))","\"fullname\"":"$NAME","\"nickname\"":"$NICKNAME","\"twoauth\"":false,"\"img\"":"$PICTURE","\"elo\"":$RANDOM}
 	curl -X POST http://localhost:3000/api/users/insert -H "Content-Type: application/json" -d "$TMP"
 done
 
