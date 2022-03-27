d163903e4a4c2ee42aeb45a4a695f6fa76dd0e77
Ledda Bryan <louchebem06@gmail.com>
2022-03-27 22:53:39 +0200

    FIX: Error /api/users/undefined

--- nestjs/src/users/users.service.ts
+++ nestjs/src/users/users.service.ts
@@ -4,7 +4,6 @@ import { Repository } from 'typeorm';
 import { Users } from './entity/users.entity';
 import { UsersInterface } from './interfaces/users.interface';
 import twofactor, { generateSecret, verifyToken } from "node-2fa";
-import { FastifyRequest } from 'fastify';
 
 @Injectable()
 export class UsersService {
@@ -39,18 +38,6 @@ export class UsersService {
 		if (user_interface?.id === undefined)
 			return ;
 		const userUpdate = await this.usersRepository.findOne({ id: +user_interface.id })
-		// if (user_interface.fullname !== undefined)
-		// 	userUpdate.fullname = user_interface.fullname;
-		// if (user_interface.nickname !== undefined)
-		// 	userUpdate.nickname = user_interface.nickname;
-		// if (user_interface.twoauth !== undefined)
-		// 	userUpdate.twoauth = user_interface.twoauth;
-		// if (user_interface.code2FA !== undefined)
-		// 	userUpdate.code2FA = user_interface.code2FA;
-		// if (user_interface.img !== undefined)
-		// 	userUpdate.img = user_interface.img;
-		// if (user_interface.elo !== undefined)
-		// 	userUpdate.elo = user_interface.elo;
 		for (const key in userUpdate)
 		{
 			if (key !== "id" && key in user_interface)
@@ -63,7 +50,16 @@ export class UsersService {
 		return this.usersRepository.find();
 	}
 
+	strisdigit(id: string) : boolean
+	{
+		for (let i = 0; i < id.length; i++)
+			if (id[i] < '0' || id[i] > '9')
+				return (false);
+		return (true);
+	}
 	findOne(id: string): Promise<Users> {
+		if (id === undefined || !this.strisdigit(id))
+			return ;
 		return this.usersRepository.findOne({ id: +id });
 	}
 
