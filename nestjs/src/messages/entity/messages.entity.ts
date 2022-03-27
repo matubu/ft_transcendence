8a123c29ad76b7ab6a2da0e40aaf813f43a09db0
Ledda Bryan <louchebem06@gmail.com>
2022-03-28 00:19:10 +0200

    FIX: message entity

--- nestjs/src/messages/entity/messages.entity.ts
+++ nestjs/src/messages/entity/messages.entity.ts
@@ -1,9 +1,9 @@
-import { Entity, Column, PrimaryColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Messages
 {
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
