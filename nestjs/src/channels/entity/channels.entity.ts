4232e30fcc6fbda57f1271bad8ca5713d1d1ba9c
Ledda Bryan <louchebem06@gmail.com>
2022-03-28 03:15:57 +0200

    FIX: entity Matchs and channels

--- nestjs/src/channels/entity/channels.entity.ts
+++ nestjs/src/channels/entity/channels.entity.ts
@@ -1,9 +1,9 @@
-import { Entity, Column, PrimaryColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Channels
 {
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn()
   id: number;
 
   @Column("int", { array: true, default: {} })
