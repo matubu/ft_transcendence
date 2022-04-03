e09b5ea93f6f4636ac89e25ba8c7b5e611da2340
Ledda Bryan <louchebem06@gmail.com>
2022-04-03 16:12:45 +0200

    FIX: quick fix windows

--- Bryan/nestjs/src/main.ts
+++ Bryan/nestjs/src/main.ts
@@ -3,21 +3,22 @@ import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import fastifyCookie from 'fastify-cookie';
 import fastifyMultipart from 'fastify-multipart';
-const fs = require('fs');
+const fs = require('fs')
 
 const { NESTJS_PORT, SECRET_COOKIES } = process.env
 
 async function bootstrap()
 {
 	const httpsOptions = {
-		key: fs.readFileSync('../key.pem'),
-		cert: fs.readFileSync('../cert.pem'),
+		key: fs.readFileSync('./key.pem'),
+		cert: fs.readFileSync('./cert.pem'),
 	};
 
 	const app = await NestFactory.create<NestFastifyApplication>(
 		AppModule,
-		new FastifyAdapter({ https: httpsOptions }),
+		new FastifyAdapter( false ? { https: httpsOptions } : {}),
 	);
+
 	app.setGlobalPrefix('api');
 	app.register(fastifyMultipart);
 	app.register(fastifyCookie, { secret: SECRET_COOKIES });
