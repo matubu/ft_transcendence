7da57a43f679a8d497e7cf2b4ce2f5e818521a4c
Mathias Berger--fruhinsholz <mathias@socialcompare.com>
2022-04-07 15:17:13 +0200

    remove

--- sapper/src/routes/chat/[id].svelte
+++ sapper/src/routes/chat/[id].svelte
@@ -9,7 +9,7 @@
 	let messages = writable([])
 
 	const reloadChat = async () => {
-		let res = await fetch(`/api/message/${id_channel}`)
+		let res = await fetch(`/api/channel/${id_channel}`)
 		if (!res.ok) return ;
 		let json = await res.json()
 		messages.set(json)
@@ -34,19 +34,21 @@
 	{/each}
 
 	<form on:submit={async e => {
-		e.preventDefault()
-		await fetch('/api/message/send', {
-			method: "POST",
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				id_channel,
-				msg: msg.value
-			})
-		})
-		msg.value = ''
-		reloadChat()
+		// TODO new send message
+
+		// e.preventDefault()
+		// await fetch('/api/message/insert', {
+		// 	method: "POST",
+		// 	headers: {
+		// 		'Content-Type': 'application/json'
+		// 	},
+		// 	body: JSON.stringify({
+		// 		id_channel,
+		// 		msg: msg.value
+		// 	})
+		// })
+		// msg.value = ''
+		// reloadChat()
 	}}>
 		<input type="text" bind:this={msg}>
 		<Button primary>Send</Button>
