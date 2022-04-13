526705ec8973cf10f89d0b24c69bdea758861076
louchebem06 <bledda@student.42nice.fr>
2022-04-13 15:45:06 +0200

    UPDATE: go jesus

--- nestjs/src/notification/notification.controller.ts
+++ nestjs/src/notification/notification.controller.ts
@@ -1,40 +1,33 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Put } from '@nestjs/common';
 import { Autorization } from 'src/auth.guard';
+import { DeleteResult, UpdateResult } from 'typeorm';
 import { Notification } from './notification.entity';
 import { NotificationService } from './notification.service';
 
 @Controller('notification')
 export class NotificationController {
 	constructor(private readonly notificationService: NotificationService) {}
+	@Post()
+	async read(@Autorization() userId: number, @Body() body: {notification_id: number}): Promise<Notification>
+	{
+		return await this.notificationService.markReadOne(body.notification_id);
+	}
 
-	// @Get()
-	// async get(@Autorization() userId: number): Promise<Notification[]>
-	// {
-	// 	return await this.notificationService.find(userId);
-	// }
+	@Put()
+	async readAll(@Autorization() userId: number): Promise<UpdateResult>
+	{
+		return await this.notificationService.markReadAll(userId);
+	}
 
-	// @Post()
-	// async read(@Autorization() userId: number, @Body() body: {notification_id: number}): Promise<Notification>
-	// {
-	// 	return await this.notificationService.markReadOne(body.notification_id);
-	// }
+	@Post()
+	async remove(@Autorization() userId: number, @Body() body: {notification_id: number}): Promise<DeleteResult>
+	{
+		return await this.notificationService.removeNotification(body.notification_id);
+	}
 
-	// @Post("readAll")
-	// async readAll(@Autorization() userId: number): Promise<IDK>
-	// {
-	// 	return await this.notificationService.markReadAll(userId);
-	// }
-
-	// @Post()
-	// async remove(@Autorization() userId: number, @Body() body: {notification_id: number}): Promise<Notification>
-	// {
-	// 	return await this.notificationService.removeNotification(body.notification_id);
-	// }
-
-	// @Post("removeAll")
-	// async removeAll(@Autorization() userId: number): Promise<IDK>
-	// {
-	// 	return await this.notificationService.removeAll(userId);
-	// }
-
+	@Put()
+	async removeAll(@Autorization() userId: number): Promise<DeleteResult>
+	{
+		return await this.notificationService.removeAll(userId);
+	}
 }
