c943d7344b9f44a2ac0268aef06536cc7a4069b7
louchebem06 <bledda@student.42nice.fr>
2022-04-15 17:54:23 +0200

    UPDATE: add achievement

--- nestjs/src/match/match.subscriber.ts
+++ nestjs/src/match/match.subscriber.ts
@@ -16,6 +16,21 @@ export class MatchSubscriber implements EntitySubscriberInterface<Match> {
 	}
 
 	async afterUpdate(event: UpdateEvent<Match>): Promise<void> {
-		console.log(event.entity);
+		const idPlayer1 = event.entity.player1;
+		const idPlayer2 = event.entity.player2;
+		const victoryPlayer1 = await this.matchService.getVictorys(idPlayer1);
+		const victoryPlayer2 = await this.matchService.getVictorys(idPlayer2);
+		const defaitePlayer1 = await this.matchService.getDefaites(idPlayer1);
+		const defaitePlayer2 = await this.matchService.getDefaites(idPlayer2);
+		this.userAchievementService.insert(idPlayer1, "Small player");
+		this.userAchievementService.insert(idPlayer2, "Small player");
+		if (victoryPlayer1.length >= 42)
+			this.userAchievementService.insert(idPlayer1, "Win");
+		if (victoryPlayer2.length >= 42)
+			this.userAchievementService.insert(idPlayer2, "Win");
+		if (defaitePlayer1.length >= 42)
+			this.userAchievementService.insert(idPlayer1, "Losing");
+		if (defaitePlayer2.length >= 42)
+			this.userAchievementService.insert(idPlayer2, "Losing");
 	}
 }
