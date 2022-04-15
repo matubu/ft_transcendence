fb2174bc74a492574e5f814aa4b934f1aee97d07
Mathias Berger--fruhinsholz <mathias@socialcompare.com>
2022-04-15 15:18:57 +0200

    friends

--- sapper/src/routes/user/[id].svelte
+++ sapper/src/routes/user/[id].svelte
@@ -3,6 +3,7 @@
 	import User from '@components/User.svelte'
 	import Button from '@components/Button.svelte'
 	import { user } from '@lib/store'
+	import { fetchUser } from '@lib/utils'
 
 	async function load() {
 		const res = await fetch(`/api/user/${+location.pathname.split('/')[2]}`)
@@ -22,21 +23,31 @@
 		<h1>{data.nickname ?? data.fullname.split(' ')[0]}</h1>
 		<p>Fullname: {data.fullname}</p>
 		<p>Elo: {data.elo}</p>
-		<Button primary on:click={() => {
-			fetch('/api/friend', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify({ friend: data.id })
-			})
-		}}>
-			{#if (true)}
-				Add friend
+		{#if $user?.id !== data.id}
+			{#if ($user.friends.find(({ friend }) => friend.id === data.id))}
+			<Button primary on:click={async () => {
+				await fetch('/api/friend', {
+					method: 'DELETE',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({ friend: data.id })
+				})
+				fetchUser()
+			}}>Remove friend</Button>
 			{:else}
-				Remove friend
+				<Button primary on:click={async () => {
+					await fetch('/api/friend', {
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json'
+						},
+						body: JSON.stringify({ friend: data.id })
+					})
+					fetchUser()
+				}}>Add friend</Button>
 			{/if}
-		</Button>
+		{/if}
 	{:catch err}
 		<p>Error: {err}</p>
 	{/await}
