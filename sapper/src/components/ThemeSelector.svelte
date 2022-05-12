18ca90088e342a9065dff4a8aa589d2d6fe4c80e
Mathias Berger--fruhinsholz <mathias@socialcompare.com>
2022-05-12 21:06:07 +0200

    style theme selector

--- sapper/src/components/ThemeSelector.svelte
+++ sapper/src/components/ThemeSelector.svelte
@@ -4,19 +4,40 @@
 	$: localStorage.setItem('theme', theme)
 </script>
 
-<div>
+<style>
+	input {
+		display: none;
+	}
+	span {
+		display: block;
+		cursor: pointer;
+		padding: 10px;
+		border: 1px solid var(--bord);
+		border-radius: 5px;
+		background: var(--bhov);
+	}
+	span:hover { background: var(--hove) }
+	span:active { background: var(--acti) }
+	input:checked + span {
+		border-color: #557cff;
+		background: #25f5;
+		color: #7f9cff;
+	}
+</style>
+
+<div class="flex">
 	<label>
 		<input type=radio bind:group={theme} value="90">
-		90
+		<span>90</span>
 	</label>
 
 	<label>
 		<input type=radio bind:group={theme} value="beach">
-		Beach
+		<span>Beach</span>
 	</label>
 
 	<label>
 		<input type=radio bind:group={theme} value="pocker">
-		Pocker
+		<span>Pocker</span>
 	</label>
 </div>
